e6819242dcafdb227df998b1036c42da670bc51f
raminrez <ramin12207@gmail.com>
2019-07-07 11:05:07 +0430

    Create note actionCreatoe

---
+++ src/actions/note.js
@@ -0,0 +1,26 @@
+import uuid from "uuid/v4";
+
+import { NOTE_ADD, NOTE_DELETE, NOTE_EDIT } from "../constants/actionTypes";
+
+const doAddNote = value => {
+  const id = uuid();
+  return {
+    type: NOTE_ADD,
+    note: {
+      id,
+      value
+    }
+  };
+};
+
+const doDeleteNote = id => ({
+  type: NOTE_DELETE,
+  note: { id }
+});
+
+const doEditNot = (id, value) => ({
+  type: NOTE_EDIT,
+  note: { id, value }
+});
+
+export { doAddNote, doDeleteNote, doEditNot };
